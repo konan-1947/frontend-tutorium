d2bb46a16c2e96c909f90c70b0b10c756f401cb9
vth2004 <vutuanhiep9099@gmail.com>
2025-04-09 20:39:38 +0700

    debug

--- src/components/becometutor/TutorRegister/Main.jsx
+++ src/components/becometutor/TutorRegister/Main.jsx
@@ -6,13 +6,12 @@ import ProgressBar from "./Processbar";
 import StepAbout from "./StepAbout";
 import StepPhoto from "./StepPhoto";
 import StepCertification from "./StepCertification";
-
 import StepDescription from "./StepDescription";
 import StepVideo from "./StepVideo";
 import StepAvailability from "./StepAvailability";
 import StepPricing from "./StepPricing";
 import { Container, Row, Col } from "react-bootstrap";
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const TutorRegister = () => {
   const [step, setStep] = useState(1);
@@ -20,46 +19,55 @@ const TutorRegister = () => {
   const location = useLocation();
 
   useEffect(() => {
+    console.log("Component mounted. Current step:", step);
+
     const handleBeforeUnload = (event) => {
       const confirmationMessage = "Bạn có chắc chắn muốn rời khỏi trang? Dữ liệu chưa lưu sẽ bị mất!";
       event.returnValue = confirmationMessage;
+      console.log("beforeunload triggered");
       return confirmationMessage;
     };
 
     window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => {
+      console.log("Component unmounted");
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
 
-  // Chặn người dùng rời khỏi trang khi bấm nút quay lại hoặc chuyển route
   useEffect(() => {
     const handleRouteChange = (event) => {
+      console.log("Route change attempted");
       const confirmLeave = window.confirm("Bạn có chắc chắn muốn rời khỏi trang? Dữ liệu chưa lưu sẽ bị mất!");
       if (!confirmLeave) {
         event.preventDefault();
         navigate(location.pathname, { replace: true });
+        console.log("Navigation prevented");
+      } else {
+        console.log("Navigation allowed");
       }
     };
 
     window.addEventListener("popstate", handleRouteChange);
-    
+
     return () => {
       window.removeEventListener("popstate", handleRouteChange);
     };
-  }, [navigate, location.pathname]);  const nextStep = () => {
+  }, [navigate, location.pathname]);
+
+  const nextStep = () => {
     if (step < 7) {
+      console.log("Moving to next step:", step + 1);
       setStep(step + 1);
-      setIsFormDirty(true);
       localStorage.setItem("savedStep", step + 1);
     }
   };
 
   const prevStep = () => {
     if (step > 1) {
+      console.log("Moving to previous step:", step - 1);
       setStep(step - 1);
-      setIsFormDirty(true);
       localStorage.setItem("savedStep", step - 1);
     }
   };
@@ -67,10 +75,11 @@ const TutorRegister = () => {
   return (
     <div className="body-container">
       <Container fluid className="tutor-register-container">
-      <ProgressBar step={step} setStep={setStep} /> {/* Truyền setStep vào */}
+        <ProgressBar step={step} setStep={(newStep) => {
+          console.log("Progress bar clicked, setting step to:", newStep);
+          setStep(newStep);
+        }} />
         <Row>
- 
-          {/* Cột phải: Nội dung từng bước */}
           <Col className="step-column">
             {step === 1 && <StepAbout nextStep={nextStep} />}
             {step === 2 && <StepPhoto nextStep={nextStep} prevStep={prevStep} />}
@@ -85,4 +94,5 @@ const TutorRegister = () => {
     </div>
   );
 };
+
 export default TutorRegister;
